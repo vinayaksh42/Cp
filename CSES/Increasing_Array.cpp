f85530e02d97c232098d49df5364d67c4fce93ab
Vinayak Sharma <vinayaksh42@gmail.com>
2021-02-01 02:50:41 +0530

    increasing array

---
+++ CSES/Increasing_Array.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ll long long
+// For loop - REP(i,1,n)
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
+using namespace std;
+
+int main() {
+    // for making input and output more efficient 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll i,n,count=0,val,prev;
+    cin >> n;
+    cin >> prev;
+    REP(i,1,n-1){
+        cin >> val;
+        if(val < prev){
+            count=count+(prev-val);
+        }else{
+            prev=val;
+        }
+        
+    }
+    cout << count;
+    return 0;
+}
