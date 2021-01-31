62cc71ec5062c6736de14fd53aecc26a41dd9913
Vinayak Sharma <vinayaksh42@gmail.com>
2021-01-31 23:20:25 +0530

    template update

--- Basic_template.cpp
+++ Basic_template.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>
+#define F first
+#define S second
+#define PB push_back
+#define MP make_pair
+#define ll long long
+// For loop - REP(i,1,n)
+#define REP(i,a,b) for (int i = a; i <= b; i++)
+
 using namespace std;
+
 int main() {
     // for making input and output more efficient 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
+
     return 0;
 }
