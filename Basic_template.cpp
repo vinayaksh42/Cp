985ea0061fc01fbad5d40f84cb8e04d6cd95ee7a
Vinayak Sharma <vinayaksh42@gmail.com>
2021-01-31 23:00:57 +0530

    template

---
+++ Basic_template.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    // for making input and output more efficient 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    return 0;
+}
